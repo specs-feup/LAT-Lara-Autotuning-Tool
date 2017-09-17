660b675509f818d28aba6fa01de05b777ae4983a
msramalho <up201403027@fe.up.pt>
2017-09-17 01:35:01 +0100

    omp isat example source code

---
+++ examples/Omp/Src/omp.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+#include <omp.h>
+
+using namespace std;
+
+#define N 100000000
+typedef double FLOAT;
+
+FLOAT Compute(FLOAT a, FLOAT b)
+{
+    FLOAT c = a * a + b * b + 2 * a * b;
+    FLOAT d = a * c + b * c + 2 * a * b * c;
+    return c / (d + 1.0);
+}
+
+int main(int argc, char **argv)
+{
+    FLOAT *A = new FLOAT[N];
+    assert(A);
+
+    FLOAT *B = new FLOAT[N];
+    assert(B);
+
+    FLOAT *C = new FLOAT[N];
+    assert(C);
+
+    // Initialization
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = i;
+        B[i] = i;
+        C[i] = 0;
+    }
+
+// Computation
+/*
+   Following demonstrates tuning omp parameters that are predefined in Isat.
+   It also demonstrates the use of dependent search.
+*/
+#pragma isat tuning scope(M_begin, M_end) measure(M_begin, M_end) variable(@omp_schedule_type, [static, dynamic, guided]) variable(@omp_schedule_chunk, range(5, 10, 1, pow2)) variable(@omp_num_threads, range(1, $NUM_CPU_THREADS, 1)) search(dependent)
+
+#pragma lara marker OmpMeasure
+{
+    int i;
+
+#pragma omp parallel for
+    for (i = 0; i < N; i++)
+    {
+        C[i] = Compute(A[i], B[i]);
+    }
+
+#pragma omp parallel for schedule(guided)
+    for (i = 0; i < N; i++)
+    {
+        C[i] = Compute(A[i], B[i]);
+    }
+}
+
+    // Gather result
+    FLOAT sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += C[i];
+    }
+    cout << "sum = " << sum << endl;
+}
